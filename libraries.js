65eff79de963918f41e11bce4424bb2f9e1280cd
Manny <gonzalezjime@wisc.edu>
2024-12-14 13:22:45 -0600

    added images and fixed library list, as well as added about bio

--- libraries.js
+++ libraries.js
@@ -31,11 +31,13 @@ fetch("libraryData.json")
 
         const cardHTML = `<!-- ${element.name} -->
       <div class="col-12 col-md-4 col-lg-2">
-        <div class="card">
-            <img src="https://via.placeholder.com/150" class="card-img-top" alt="${element.name} Library Image">
+        <div class="card" id="card">
+            <div class="image-container">
+              <img src="${element.image}" class="card-img-top" alt="${element.name} Library Image">
+            </div>
             <div class="card-body">
                 <h5 class="card-title">${element.name}</h5>
-                <a href="location.html#${sanitizedID}Details" class="btn btn-danger">Explore</a>
+                <a href="location.html#${sanitizedID}Details" class="btn btn-danger" id="collapseButton">Explore</a>
           </div>
         </div>
       </div>
@@ -74,6 +76,17 @@ fetch("libraryData.json")
 
         count += 1;
       });
+
+      document
+        .getElementById("collapseButton")
+        .addEventListener("click", function () {
+          // Toggle the aria-expanded attribute based on the current state
+          const isExpanded = this.getAttribute("aria-expanded") === "true";
+          // Save the state to localStorage (true for expanded, false for collapsed)
+          localStorage.setItem("collapseState", !isExpanded);
+          // Update the aria-expanded attribute
+          this.setAttribute("aria-expanded", !isExpanded);
+        });
     } else if (mainLibraries) {
       // ForEach loop to write to the locations
       data.forEach((element) => {
@@ -90,38 +103,69 @@ fetch("libraryData.json")
           ${element.hours.today}
       </div>
       <div>
-          <a href="#" class="btn btn-link">Books</a>
-          <a href="#" class="btn btn-link">Events</a>
+          <a href="/book.html" class="btn btn-link">Books</a>
+          <a href="/event.html" class="btn btn-link">Events</a>
           <button class="btn btn-light dropdown-toggle" type="button" data-bs-toggle="collapse"
               data-bs-target="#${sanitizedID}Details" aria-expanded="false" aria-controls="${sanitizedID}Details"></button>
+          
+              <script>
+              document.getElementById('collapseButton').addEventListener('click', function() {
+                // Toggle the aria-expanded attribute based on the current state
+                const isExpanded = this.getAttribute('aria-expanded') === 'true';
+                // Save the state to localStorage (true for expanded, false for collapsed)
+                localStorage.setItem('collapseState', !isExpanded);
+                // Update the aria-expanded attribute
+                this.setAttribute('aria-expanded', !isExpanded);
+              });
+            </script>
       </div>
   </div>
   <div class="collapse" id="${sanitizedID}Details">
-      <div class="card card-body">
-          Additional details about ${element.name}:
-
-          
-      </div>
-  </div>`;
+    <div class="card card-body">
+        <div class="row">
+            <!-- Left Column: Text Details -->
+            <div class="col-md-6">
+                <strong>Additional details about ${element.name}:</strong>
+                <br>
+                Location: ${element.location}<br>
+                Contact: ${element.contact.phone} or ${
+          element.contact.email
+        }<br>
+                <br>
+                Services: ${element.services.join(", ")}<br>
+                <br>
+                Specialties: ${element.specialties.join(", ")}<br>
+            </div>
+            <!-- Right Column: Image -->
+            <div class="col-md-6 text-center">
+                <div class="image-container">
+                    <img src="${
+                      element.image
+                    }" class="card-img-top img-fluid" alt="${
+          element.name
+        } Library Image">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>`;
 
         mainLibraries.innerHTML += collapsable;
       });
+
+      // Retrieve the collapse state from localStorage
+      const collapseState = localStorage.getItem("collapseState");
+      const collapseButton = document.getElementById("collapseButton");
+      const collapseElement = document.getElementById("${sanitizedID}Details");
+
+      if (collapseState === "true") {
+        // If the state is true (expanded), expand the collapse
+        collapseElement.classList.add("show");
+        collapseButton.setAttribute("aria-expanded", "true");
+      }
+
+      // Optionally, clear the state from localStorage if you want to reset the state later
+      // localStorage.removeItem('collapseState');
     }
   })
   .catch((error) => console.error("Error loading the JSON file:", error));
-
-document.addEventListener("DOMContentLoaded", function () {
-  // If there's a hash in the URL (e.g., #AMP-Library-details)
-  if (window.location.hash) {
-    const hash = window.location.hash;
-
-    const targetElement = document.querySelector(hash);
-
-    if (targetElement) {
-      const dropdownButton = targetElement.querySelector(".dropdown-toggle");
-      if (dropdownButton) {
-        $(dropdownButton).collapse("show");
-      }
-    }
-  }
-});
